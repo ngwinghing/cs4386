8959fd1166b73f34f304ee2bfbeff67bf41ecb12
ngwinghing <wing.nwh@gmail.com>
2017-11-27 00:23:18 +0800

    Update playerObject.js

--- js/playerObject.js
+++ js/playerObject.js
@@ -1,13 +1,1 @@
-/**
- * Created by ngwinghing on 25/11/2017.
- */
-function i2xy(index) {
-    var x = index % mapWidth;
-    var y = Math.floor(index/mapWidth);
 
-    return [x,y];
-}
-
-function xy2i (x, y, mapWidth) {
-    return y*mapWidth +x;
-}
